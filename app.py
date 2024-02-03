d66464d426ad21e36c099a5348ffdeb68065c377
islem-habibi <islemhabibi3@gmail.com>
2024-02-03 12:51:15 +0100

    model.pkl

--- app.py
+++ app.py
@@ -56,4 +56,4 @@ def predict_proba():
 
 # Exécuter l'application Flask sur le port 8000
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=8000, debug=True)
+    app.run(host='0.0.0.0', port=7000, debug=True)
