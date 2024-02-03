5f1551f5e1dc04e59c8a1e4a28eec6428eec0ac8
islem-habibi <islemhabibi3@gmail.com>
2024-02-03 14:21:15 +0100

    app added 2 methods, GET and POST

--- app.py
+++ app.py
@@ -19,38 +19,38 @@ app = Flask(__name__)
 
 
 # Définir une route pour effectuer des prédictions
-@app.route('/', methods=['POST'])
-def predict_proba():
-    try:
-        # Obtenir les données JSON de la requête
-        data_df = request.get_json(force=True)
-        #df = pd.DataFrame([data_df]) 
-        
-        df= pd.DataFrame([data_df["data"]], columns=data_df["keys"])#data_df["data"]
-        feature_names = data_df["keys"]
-        # Effectuer la prédiction
-        prediction = model.predict_proba(df)[:, 1]
-        prediction = (prediction*100).round(2)
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        try:
+            # Obtenir les données JSON de la requête
+            data_df = request.get_json(force=True)
+            df = pd.DataFrame([data_df["data"]], columns=data_df["keys"])
+            feature_names = data_df["keys"]
+            # Effectuer la prédiction
+            prediction = model.predict_proba(df)[:, 1]
+            prediction = (prediction*100).round(2)
 
-        explainer = shap.Explainer(model)
-        shap_values = explainer.shap_values(df)
+            explainer = shap.Explainer(model)
+            shap_values = explainer.shap_values(df)
 
-        # Créer un plot SHAP et le convertir en image PNG encodée en base64
-        plt.figure()
-        shap.force_plot(explainer.expected_value, shap_values[0], feature_names=feature_names, matplotlib=True)
-        plt.tight_layout()
-        img = io.BytesIO()
-        plt.savefig(img, format='png')
-        plt.close()
-        img.seek(0)
-        plot_data = base64.b64encode(img.getvalue()).decode()
+            # Créer un plot SHAP et le convertir en image PNG encodée en base64
+            plt.figure()
+            shap.force_plot(explainer.expected_value, shap_values[0], feature_names=feature_names, matplotlib=True)
+            plt.tight_layout()
+            img = io.BytesIO()
+            plt.savefig(img, format='png')
+            plt.close()
+            img.seek(0)
+            plot_data = base64.b64encode(img.getvalue()).decode()
 
-        # Renvoyer les prédictions au format JSON
-        #return jsonify(f"le df est{df}")
+            # Renvoyer les prédictions au format JSON
+            return jsonify({'prediction': prediction.tolist(), 'shap_plot': 'data:image/png;base64,' + plot_data})
+        except Exception as e:
+            return jsonify({'error': str(e)})
+    else:
+        # Servir la page HTML pour les requêtes GET
         return render_template('index.html')
-        #return jsonify({'prediction': prediction.round(2).tolist(), 'shap_plot': 'data:image/png;base64,' + plot_data})
-    except Exception as e:
-        return jsonify({'error': str(e), 'df': df})
 
 # Exécuter l'application Flask sur le port 7000
 if __name__ == '__main__':
